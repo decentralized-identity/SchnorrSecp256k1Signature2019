08ccac1723018b21097a542b78e8b6609a6e9c12
Orie Steele <orie@transmute.industries>
2020-05-02 15:59:32 -0500

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as SchnorrSecp256k1VerificationKey2019 } from './SchnorrSecp256k1VerificationKey2019';
 export { default as SchnorrSecp256k1Signature2019 } from './SchnorrSecp256k1Signature2019';
+export { default as SS256K } from './SS256K';
 export { default as keyUtils } from './keyUtils';
 export { default as documentLoader } from './defaultDocumentLoader';
